5f537f40dc5f1894b347836dc5046c5fd7921659
veeexy <marasek515@gmail.com>
2023-04-19 22:07:47 +0200

    removing bugs

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 from website import create_ticket
 
@@ -9,9 +10,11 @@ views = Blueprint('views', __name__)
 # def test():
 #     # example how to use (creates ticket when on home page)
 #     create_ticket(3)
+@views.route('/')
 def home():
     return render_template("home.html")
 
 @views.route('/admin')
+@login_required
 def admin():
     return render_template("admin.html")
